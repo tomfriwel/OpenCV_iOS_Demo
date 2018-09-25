45e4574596f2d1ddb7e67c6cdc823d2a7a8595a3
tomfriwel <976932447@qq.com>
2018-09-25 16:52:16 +0800

    add canny edge detect

---
+++ OpenCV_iOS_Demo/EdgeViewController.h
@@ -0,0 +1,17 @@
+//
+//  EdgeViewController.h
+//  OpenCV_iOS_Demo
+//
+//  Created by tomfriwel on 2018/9/25.
+//  Copyright © 2018 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EdgeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OpenCV_iOS_Demo/EdgeViewController.m
@@ -0,0 +1,32 @@
+//
+//  EdgeViewController.m
+//  OpenCV_iOS_Demo
+//
+//  Created by tomfriwel on 2018/9/25.
+//  Copyright © 2018 tomfriwel. All rights reserved.
+//
+
+#import "EdgeViewController.h"
+
+@interface EdgeViewController ()
+
+@end
+
+@implementation EdgeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
